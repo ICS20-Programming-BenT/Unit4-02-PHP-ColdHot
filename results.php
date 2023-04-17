60afa225e211e328d71c5aa6fb1280c9de303ab6
BenThomson2007 <124379717+BenThomson2007@users.noreply.github.com>
2023-04-17 15:02:56 +0000

    Fixed more errors

--- results.php
+++ results.php
@@ -7,12 +7,12 @@
 
 //if: if temperature is 15 degrees or under
 if ($temperature <= TEMP_LIMIT) {
-  $message = "It is chilly out! I suggest you wear some kind of jacket to stay warm.";
-} 
+    $message = "It is chilly out! I suggest you wear some kind of jacket to stay warm.";
+}
 //else: if temperature is more than 15 degrees
 else {
-  $message = "It is pretty hot out! You might want to wear sunscreen or a baseball cap.";
-} 
+    $message = "It is pretty hot out! You might want to wear sunscreen or a baseball cap.";
+}
   //displaying the correct message to the screen
   echo "$message";
 ?>

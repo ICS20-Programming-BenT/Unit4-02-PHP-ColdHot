89302df4fec46787fe0de745903d52c990212692
BenThomson2007 <124379717+BenThomson2007@users.noreply.github.com>
2023-04-17 14:42:06 +0000

    Fixed errors

--- results.php
+++ results.php
@@ -2,14 +2,14 @@
 
   // Define temperature and message variables & temperature limit constant
   $temperature = floatval($_POST["temp-input"]);
-  define ("TEMP_LIMIT", 15);
+  define("TEMP_LIMIT", 15);
   $message = "";
 
   //if: if temperature is 15 degrees or under
   if ($temperature <= TEMP_LIMIT) {
     $message = "It is chilly out! I suggest you wear some kind of jacket to stay warm.";
-  }
-    
+  } 
+
   //else: if temperature is more than 15 degrees
   else {
     $message = "It is pretty hot out! You might want to wear sunscreen or a baseball cap.";
